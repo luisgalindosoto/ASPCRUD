6020fab85a17e7843604bdc940dad79362ac6060
luisgalindosoto <73960357+luisgalindosoto@users.noreply.github.com>
2023-10-03 10:00:01 -0600

    PRIMER COMIT

---
+++ CRUD/Pages/CRUD.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MP.Master" AutoEventWireup="true" CodeBehind="CRUD.aspx.cs" Inherits="CRUD.Pages.CRUD" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+    CRUD
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+    <br />
+    <div class="mx-auto" style="width:250px">
+        <asp:Label runat="server" CssClass="h2" ID="lbltitulo"></asp:Label>
+    </div>
+    <form runat="server" class="h-100 d-flex aling-items-center justify-content-center">
+       <div>
+              <div class="bm-3">
+      <label class="form-label">Nombre</label>
+      <asp:TextBox runat="server" CssClass="form-control" ID="tbnombre"></asp:TextBox>
+      </div>
+      <div class="bm-3">
+          <label class="form-label">Edad</label>
+          <asp:TextBox runat="server" CssClass="form-control" ID="tbedad"></asp:TextBox>
+      </div>
+      <div class="bm-3">
+          <label class="form-label">Email</label>
+          <asp:TextBox runat="server" CssClass="form-control" ID="tbmail"></asp:TextBox>
+      </div>
+      <div class="bm-3">
+          <label class="form-label">Fecha De Nacimiento</label>
+          <asp:TextBox runat="server" TextMode="Date" CssClass="form-control" ID="tbdate"></asp:TextBox>
+      </div>
+      <asp:Button  runat="server" CssClass="btn btn-primary" ID="BtnCreate" Text="Create" Visible="false" OnClick="BtnCreate_Click"/>
+      <asp:Button  runat="server" CssClass="btn btn-primary" ID="BtnUpdate" Text="Update" Visible="false" OnClick="BtnUpdate_Click"/>
+      <asp:Button  runat="server" CssClass="btn btn-primary" ID="BtnDelete" Text="Delete" Visible="false" OnClick="BtnDelete_Click"/>
+      <asp:Button  runat="server" CssClass="btn btn-primary btn-dark" ID="BtnVolver" Text="Volver" Visible="true" OnClick="BtnVolver_Click" />
+
+      </div>
+
+      
+    </form>
+</asp:Content>
---
+++ CRUD/Pages/CRUD.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web.UI.WebControls;
+namespace CRUD.Pages
+{
+    public partial class CRUD : System.Web.UI.Page
+    {
+        readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+        public static string sID = "-1";
+        public static string sOpc = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Obtener el id
+            if (!Page.IsPostBack)
+            {
+                if (Request.QueryString["id"] != null)
+                {
+                    sID = Request.QueryString["id"].ToString();
+                    CargarDatos();
+                    tbdate.TextMode = TextBoxMode.DateTime;
+                }
+                if (Request.QueryString["op"] != null)
+                {
+                    sOpc = Request.QueryString["op"].ToString();
+
+                    switch (sOpc)
+                    {
+                        case "C":
+                            this.lbltitulo.Text = "Ingresar Nuevo Usuario";
+                            BtnCreate.Visible = true;
+                            break;
+                        case "R":
+                            this.lbltitulo.Text = "Consulta de usuario";
+                            break;
+                        case "U":
+                            this.lbltitulo.Text = "Modificar Usuario";
+                            this.BtnUpdate.Visible = true;
+                            break;
+                        case "D":
+                            this.lbltitulo.Text = "Eliminiar Usuario";
+                            this.BtnDelete.Visible = true;
+                            break;
+
+                    }
+
+                }
+            }
+        }
+
+        void CargarDatos()
+        {
+
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("sp_read", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.Add("@Id", SqlDbType.Int).Value = sID;
+            DataSet ds = new DataSet();
+            ds.Clear();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            DataRow row = dt.Rows[0];
+            tbnombre.Text = row[1].ToString();
+            tbedad.Text = row[2].ToString();
+            tbmail.Text = row[3].ToString();
+            DateTime d = (DateTime)row[4];
+            tbdate.Text = d.ToString("dd/MM/yyy");
+            con.Close();
+
+        }
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_create", con);
+            con.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = tbnombre.Text;
+            cmd.Parameters.Add("@Edad", SqlDbType.Int).Value = tbedad.Text;
+            cmd.Parameters.Add("@Correo", SqlDbType.VarChar).Value = tbmail.Text;
+            cmd.Parameters.Add("@Fecha", SqlDbType.VarChar).Value = tbdate.Text;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Redirect("Index.aspx");
+
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_update", con);
+            con.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Id", SqlDbType.VarChar).Value = sID;
+            cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = tbnombre.Text;
+            cmd.Parameters.Add("@Edad", SqlDbType.Int).Value = tbedad.Text;
+            cmd.Parameters.Add("@Correo", SqlDbType.VarChar).Value = tbmail.Text;
+            cmd.Parameters.Add("@Fecha", SqlDbType.VarChar).Value = tbdate.Text;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Redirect("Index.aspx");
+
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_delete", con);
+            con.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Id", SqlDbType.VarChar).Value = sID;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Redirect("Index.aspx");
+
+        }
+
+        protected void BtnVolver_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Index.aspx");
+        }
+    }
+}
